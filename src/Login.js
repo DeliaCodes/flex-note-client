6ee6c5607cf10e5129495271ff1e39360bf3fd7a
deliacodes <deliacodes@gmail.com>
2019-02-18 00:35:49 -0800

    Login and Registration Components drafted.

---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Login(props) {
+  return (
+    <section className="mainSection">
+      <header className="banner" role="banner">
+        <h1>Login Below</h1>
+      </header>
+      <form>
+        <fieldset>
+          <label htmlFor="username">
+            Username:{" "}
+            <input id="username" defaultValue="Username" name="Username" />
+          </label>
+          <label htmlFor="password">
+            Password:{" "}
+            <input
+              id="password"
+              type="text"
+              defaultValue="Password"
+              name="Password"
+            />
+          </label>
+          <input
+            id="loginSubmit"
+            className="submit"
+            type="submit"
+            name="submit"
+            role="button"
+          />
+        </fieldset>
+      </form>
+      <a href="regis.html" className="nav">
+        Register Here
+      </a>
+    </section>
+  );
+}
---
+++ src/Regis.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Regis(props) {
+  return (
+    <section className="mainSection">
+      <header className="banner" role="banner">
+        <h1>Register Here</h1>
+      </header>
+      <form>
+        <fieldset>
+          <label htmlFor="email">
+            Email: <input id="email" defaultValue="Email" name="Email" />
+          </label>
+          <label htmlFor="username">
+            Username:{" "}
+            <input id="username" defaultValue="Username" name="Username" />
+          </label>
+          <label htmlFor="password">
+            Password:{" "}
+            <input
+              id="password"
+              type="text"
+              defaultValue="Password"
+              name="Password"
+            />
+          </label>
+          <input
+            id="registerSubmit"
+            className="submit"
+            type="submit"
+            name="submit"
+          />
+        </fieldset>
+      </form>
+      <a href="login.html" className="nav">
+        Need To Login?
+      </a>
+    </section>
+  );
+}
