e787f7ed16c5d04e08e486fb63188d4342a672f5
deliacodes <deliacodes@gmail.com>
2019-02-18 16:30:59 -0800

    correcting import from test.

--- src/__tests__/nav-auth.js
+++ src/__tests__/nav-auth.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NavAuth from "../NavAuth";
+import NavAuth from "../Nav-Auth";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
