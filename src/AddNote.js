b2f83a873987ea635e675ea3bfc469f325cc46d3
deliacodes <deliacodes@gmail.com>
2019-02-20 00:10:47 -0800

    Adding draft-js editor and state component.

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,21 +1,34 @@
 import React from "react";
-
+import { Editor, EditorState } from "draft-js";
 import "./css/AddNote.css"
 
-export default function AddNote(props) {
-  return (
-    <section>
+export default class AddNote extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = { editorState: EditorState.createEmpty()};
+  this.onChange = (editorState) => this.setState({editorState})
+}
+
+render () {
+  return <section>
       <header className="banner" role="banner">
         <h1>Add Notes Here</h1>
       </header>
-      <form>
+      <Editor editorState={this.state.editorState}
+      onChange={this.onChange}
+      />
+            {/* Template is another or different form - for low number create buttons for each template 
+            <form>
         <fieldset>
           <label htmlFor="note">
-            {/* Template is another or different form - for low number create buttons for each template */}
-            <input id="note" defaultValue="Add a Note Here" name="" />
+                   <input id="note" defaultValue="Add a Note Here" name="" />
           </label>
         </fieldset>
-      </form>
+        </form>
+            */}
+     
+      
     </section>
-  );
+
+}
 }
