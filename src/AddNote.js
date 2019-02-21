d64e0effb36631965bd05559c635391fd0ea0fa2
deliacodes <deliacodes@gmail.com>
2019-02-21 11:41:55 -0800

    Wrapping rich text editor component in a Redux form.

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,35 +1,17 @@
 import React from "react";
-import RichTextEditor from "react-rte";
-import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import FormRTE from "./Form";
+import {Field, reduxForm } from "redux-form";
 import "./css/AddNote.css"
 
-export default class AddNote extends React.Component {
-  static propTypes = {
-    onChange: PropTypes.func
-  };
-
-  state = {
-    value: RichTextEditor.createEmptyValue()
-  }
-
-  onChange = (value) => {
-    this.setState({value});
-    if (this.props.onChange) {
-      // Send the changes up to the parent component as an HTML string.
-      // This is here to demonstrate using `.toString()` but in a real app it
-      // would be better to avoid generating a string on each change.
-      this.props.onChange(
-        value.toString('html')
-      );
-    }
-  };
+export class AddNote extends React.Component {
 
 render () {
   return <section>
       <header className="banner" role="banner">
         <h1>Add Notes Here</h1>
       </header>
-      <RichTextEditor value={this.state.value} onChange={this.onChange} />
+      <Field name="description" component={FormRTE} />
       
             {/* Template is another or different form - for low number create buttons for each template 
             <form>
@@ -39,6 +21,8 @@ render () {
           </label>
         </fieldset>
         </form>
+
+        add some sort of dispatch to dispatch into state
             */}
      
       
@@ -46,3 +30,11 @@ render () {
 
 }
 }
+
+const mapStateToProps = state => ({
+value: state.value,
+})
+
+export default connect(mapStateToProps)(reduxForm({
+  form: 'AddNote'
+})(AddNote));
