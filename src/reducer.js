ea5098c193291faeec4c69842ee0fe62c2937074
deliacodes <deliacodes@gmail.com>
2019-02-23 00:51:17 -0800

    Adding Update and Remove to the reducer.

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-import {ADD_NOTE } from "./actions";
+import {ADD_NOTE, UPDATE_NOTE, DELETE_NOTE } from "./actions";
 
 const initialState ={
   notes: [],
@@ -35,11 +35,41 @@ atque fugam dedit et praeter vada fervida vexit.`
   }
 }
 
+// test me
+const updateObjectInArray = (array, noteToUpdate) => {
+  return array.map((item, index) => {
+    if (item.id === noteToUpdate.id) {
+      return {
+        ...item,
+      ...noteToUpdate,
+      }
+    }
+    return item
+  })
+}
+
+function removeItem(array, itemToDelete ) {
+  return array.filter((item) => item.id !== itemToDelete.id)
+}
+
 export const crudReducer = (state=initialState, action) => {
   if (action.type === ADD_NOTE) {
     return Object.assign({}, state, {
 notes: [...state.notes, action.note]
     });
+  }
+
+  // figure out how to hook out
+  else if (action.type === UPDATE_NOTE) {
+    return Object.assign({}, state, {
+      notes: updateObjectInArray(state.notes, action.note)
+    })
+  }
+
+  else if (action.type === DELETE_NOTE) {
+    return Object.assign({}, state, {
+      notes: removeItem(state.notes, action.note)
+    })
   }
   return state;
 };
