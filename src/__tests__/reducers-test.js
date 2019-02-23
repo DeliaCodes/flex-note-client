68a87a8306981c0e02eb1e110fad75170fb04c4a
deliacodes <deliacodes@gmail.com>
2019-02-23 01:33:07 -0800

    Adding Update, Delete reducer tests.

--- src/__tests__/reducers-test.js
+++ src/__tests__/reducers-test.js
@@ -1,5 +1,5 @@
 import {crudReducer} from "../reducer"
-import {addNote } from "../actions";
+import {addNote, updateNote, deleteNote } from "../actions";
 
 describe('Crud Reducer', () => {
 
@@ -16,7 +16,57 @@ describe('Crud Reducer', () => {
         }]
       }
 const testingState = crudReducer(state, addNote(item));
-console.log('testing state', testingState);
+// console.log('testing state', testingState);
 expect(testingState.notes).toContain(item);
+});
+it('should update a note', () => {
+  const updatedItem = {
+    id: 5,
+    title: "Hark Here I come",
+      text: "Aeneas et Troiae"
+    }
+    let state = {
+      notes: [{
+        id: 7,
+        title: "Your Testing Aeneid ",
+        text: `Vous quoque litoribus nostris, Aeneia nutrix,
+    aeternam moriens famam, Caieta, dedisti;`
+      }, {
+        id: 5,
+        title: "jester",
+        text: "jingle jingle",
+      }
+    
+    ]
+    }
+const testingState = crudReducer(state, updateNote(updatedItem));
+// console.log('testing state', testingState);
+expect(testingState.notes).not.toContain(state.notes[1])
+expect(testingState.notes).toContainEqual(updatedItem);
+});
+it('should delete a note', () => {
+  const itemToDelete = {
+    id: 5,
+    title: "jester",
+    text: "jingle jingle",
+  }
+    let state = {
+      notes: [{
+        id: 7,
+        title: "Your Testing Aeneid ",
+        text: `Vous quoque litoribus nostris, Aeneia nutrix,
+    aeternam moriens famam, Caieta, dedisti;`
+      }, {
+        id: 5,
+        title: "jester",
+        text: "jingle jingle",
+      }
+    
+    ]
+    }
+const testingState = crudReducer(state, deleteNote(itemToDelete));
+// console.log('testing state', testingState);
+expect(testingState.notes).not.toContain(state.notes[1])
+expect(testingState.notes).toContainEqual(state.notes[0]);
 });
 })
