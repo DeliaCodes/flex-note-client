7b487bea6cf3d2adf2ee5d77eadf3542b6af9747
deliacodes <deliacodes@gmail.com>
2019-02-23 01:16:46 -0800

    Removing excessive note length. Correcting posts to notes.

--- src/reducer.js
+++ src/reducer.js
@@ -4,35 +4,14 @@ const initialState ={
   notes: [],
   navUrl: "/login",
   navName: "Login",
-  posts: {
+  notes: [{
     id: 1,
     title: "Today's Aeneid Reading",
     text: `Tu quoque litoribus nostris, Aeneia nutrix,
 aeternam moriens famam, Caieta, dedisti;
 et nunc servat honos sedem tuus, ossaque nomen
-Hesperia in magna, si qua est ea gloria, signat.
-
-At pius exsequiis Aeneas rite solutis,               
-aggere composito tumuli, postquam alta quierunt
-aequora, tendit iter velis portumque relinquit.
-aspirant aurae in noctem nec candida cursus
-luna negat, splendet tremulo sub lumine pontus.
-proxima Circaeae raduntur litora terrae,   
-dives inaccessos ubi Solis filia lucos
-adsiduo resonat cantu, tectisque superbis
-urit odoratam nocturna in lumina cedrum
-arguto tenuis percurrens pectine telas.
-hinc exaudiri gemitus iraeque leonum
-vincla recusantum et sera sub nocte rudentum,
-saetigerique sues atque in praesepibus ursi
-saevire ac formae magnorum ululare luporum,
-quos hominum ex facie dea saeva potentibus herbis
-induerat Circe in vultus ac terga ferarum.
-quae ne monstra pii paterentur talia Troes
-delati in portus neu litora dira subirent,
-Neptunus ventis implevit vela secundis,
-atque fugam dedit et praeter vada fervida vexit.`
-  }
+Hesperia in magna, si qua est ea gloria, signat.`
+  }]
 }
 
 // test me
@@ -54,6 +33,7 @@ function removeItem(array, itemToDelete ) {
 
 export const crudReducer = (state=initialState, action) => {
   if (action.type === ADD_NOTE) {
+    console.log('add state', state);
     return Object.assign({}, state, {
 notes: [...state.notes, action.note]
     });
