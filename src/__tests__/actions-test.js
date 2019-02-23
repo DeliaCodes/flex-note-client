4b719dd9197f931f4dc85d3724ae4a80ebafe0fa
deliacodes <deliacodes@gmail.com>
2019-02-23 01:22:28 -0800

    Adding Update and Delete Action tests.

--- src/__tests__/actions-test.js
+++ src/__tests__/actions-test.js
@@ -1,5 +1,5 @@
 import {
-  ADD_NOTE, addNote
+  ADD_NOTE, addNote, UPDATE_NOTE, updateNote, DELETE_NOTE, deleteNote,
 } from "../actions";
 
 describe('Add Note Actions', () => {
@@ -7,6 +7,25 @@ describe('Add Note Actions', () => {
     const item ={ title: 'Horatio', text: "My left foot"}
     const testAction = addNote(item);
     expect(testAction.type).toEqual(ADD_NOTE);
+    expect(testAction.note).toEqual(item);
+  })
+})
+
+describe('Update Note Actions', () => {
+  it('Should return an update action', () => {
+    const item ={ id: 3, title: 'Sea Captain', text: "Whales Ahoy"}
+    const testAction = updateNote(item);
+    expect(testAction.type).toEqual(UPDATE_NOTE);
+    expect(testAction.note).toEqual(item);
+  });
+})
+
+
+describe('Delete Note Actions', () => {
+  it('Should return a delete action', () => {
+    const item ={ id: 5, title: 'Romeo', text: "My bloody heart"}
+    const testAction = deleteNote(item);
+    expect(testAction.type).toEqual(DELETE_NOTE);
     expect(testAction.note).toEqual(item);
   })
 })
