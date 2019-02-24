f8a214b3f02ae159b17d8fd30fedbd2a547c30be
deliacodes <deliacodes@gmail.com>
2019-02-24 15:18:46 -0800

    Working on adding enzyme.

--- src/__tests__/allNav.js
+++ src/__tests__/allNav.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import AllNav from "../AllNav";
+import store from "../store";
+import { Provider } from "react-redux";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<AllNav />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('new render for AllNav', () => {
+  it("renders without crashing", () => {
+
+    shallow(<AllNav />)
+    /* const div = document.createElement("div");
+    ReactDOM.render(<Provider store={store}> <AllNav /> </Provider>, div);
+    ReactDOM.unmountComponentAtNode(div); */
+  });
+
+})
+
+
