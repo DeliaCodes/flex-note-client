2fcfad42cd110404128f4cc17aa21db0390ec8c0
deliacodes <deliacodes@gmail.com>
2019-02-24 15:13:16 -0800

    Editing to reflect current state data stuff.

--- src/NoteFeed.js
+++ src/NoteFeed.js
@@ -1,26 +1,26 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import "./css/NoteFeed.css";
 
 export function NoteFeed(props) {
   return (
-    <section id="posts">
+    <section className="notes">
       <header className="banner" role="banner">
         <h1>My Notes</h1>
       </header>
       {/*
       is props.id right?
       */}
-      <div id={props.posts.id}>
-        <p>{props.posts.title}</p>
-        <p>{props.posts.text}</p>
+      <div id={props.notes.id}>
+        <p>{props.notes.title}</p>
+        <p>{props.notes.text}</p>
       </div>
     </section>
   );
 }
 
 const mapStateToProps = state => ({
-posts: state.posts
+  notes: state.notes
 })
 
 export default connect(mapStateToProps)(NoteFeed);
