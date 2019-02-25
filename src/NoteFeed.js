66e30751271acb85173b9da28d0f822c4f97bf30
deliacodes <deliacodes@gmail.com>
2019-02-24 18:12:59 -0800

    fixed the myNotes render.

--- src/NoteFeed.js
+++ src/NoteFeed.js
@@ -2,25 +2,40 @@ import React from "react";
 import { connect } from "react-redux";
 import "./css/NoteFeed.css";
 
+/* const generateFeed = notesArray => {
+  return notesArray.map(item => `<div className="note" id="${item.id}"><p className="title">${item.title}</p><p className="text">${item.text}</p></div>`)
+} */
+
+
 export function NoteFeed(props) {
+  const myNotes = props.notes.map(item => <div className="note" id={item.id}><p className="title">{item.title}</p><p className="text">{item.text}</p></div>)
+
   return (
     <section className="notes">
       <header className="banner" role="banner">
         <h1>My Notes</h1>
       </header>
-      {/*
-      is props.id right?
+      {/*        
       */}
-      <div id={props.notes.id}>
-        <p>{props.notes.title}</p>
-        <p>{props.notes.text}</p>
+      <div>
+        {myNotes}
       </div>
     </section>
   );
 }
+NoteFeed.defaultProps = {
+  notes: [{
+    id: 1,
+    title: "Today's Aeneid Reading",
+    text: `Tu quoque litoribus nostris, Aeneia nutrix,
+aeternam moriens famam, Caieta, dedisti;
+et nunc servat honos sedem tuus, ossaque nomen
+Hesperia in magna, si qua est ea gloria, signat.`
+  }]
+};
 
 const mapStateToProps = state => ({
-  notes: state.notes
+  notes: state.notes,
 })
 
 export default connect(mapStateToProps)(NoteFeed);
