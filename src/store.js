035538976dd5d676debc24bd68bce7bafcd8b30b
deliacodes <deliacodes@gmail.com>
2019-02-25 11:00:22 -0800

    Adding navReducer.

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
-import {createStore, combineReducers} from "redux";
-import {reducer as formReducer} from 'redux-form'
-import {crudReducer} from "./reducer"
+import { createStore, combineReducers } from "redux";
+import { reducer as formReducer } from 'redux-form'
+import { crudReducer, navReducer } from "./reducer"
 
+// form reducer is redux form
 
 export default createStore(
     combineReducers({
         form: formReducer,
         crud: crudReducer,
+        nav: navReducer
     })
 );
 
