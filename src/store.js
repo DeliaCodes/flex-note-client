572d8d603c2ec9171e9d4e4c78d03b1d81968754
deliacodes <deliacodes@gmail.com>
2019-02-27 05:21:15 -0800

    Adding Redux Dev tools to store.

--- src/store.js
+++ src/store.js
@@ -1,15 +1,24 @@
 import { createStore, combineReducers } from "redux";
 import { reducer as formReducer } from 'redux-form'
 import { crudReducer, navReducer } from "./reducer"
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // form reducer is redux form
 
-export default createStore(
+const store = createStore(
+    combineReducers({
+        form: formReducer,
+        crud: crudReducer,
+        nav: navReducer
+    }), composeWithDevTools());
+
+export default store;
+/* export default createStore(
     combineReducers({
         form: formReducer,
         crud: crudReducer,
         nav: navReducer
     })
-);
+); */
 
 
