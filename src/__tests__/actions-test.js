642a494f9d7890ba28535eed3b604526d11f8743
deliacodes <deliacodes@gmail.com>
2019-02-27 17:23:41 -0800

    Updating actions imports.

--- src/__tests__/actions-test.js
+++ src/__tests__/actions-test.js
@@ -1,10 +1,10 @@
 import {
   ADD_NOTE, addNote, UPDATE_NOTE, updateNote, DELETE_NOTE, deleteNote, NAV_LOGIN, loginNav, NAV_LOGOUT, logoutNav
-} from "../actions";
+} from "../actions/actions";
 
 describe('Add Note Actions', () => {
   it('Should return an add action', () => {
-    const item ={ title: 'Horatio', text: "My left foot"}
+    const item = { title: 'Horatio', text: "My left foot" }
     const testAction = addNote(item);
     expect(testAction.type).toEqual(ADD_NOTE);
     expect(testAction.note).toEqual(item);
@@ -13,7 +13,7 @@ describe('Add Note Actions', () => {
 
 describe('Update Note Actions', () => {
   it('Should return an update action', () => {
-    const item ={ id: 3, title: 'Sea Captain', text: "Whales Ahoy"}
+    const item = { id: 3, title: 'Sea Captain', text: "Whales Ahoy" }
     const testAction = updateNote(item);
     expect(testAction.type).toEqual(UPDATE_NOTE);
     expect(testAction.note).toEqual(item);
@@ -23,7 +23,7 @@ describe('Update Note Actions', () => {
 
 describe('Delete Note Actions', () => {
   it('Should return a delete action', () => {
-    const item ={ id: 5, title: 'Romeo', text: "My bloody heart"}
+    const item = { id: 5, title: 'Romeo', text: "My bloody heart" }
     const testAction = deleteNote(item);
     expect(testAction.type).toEqual(DELETE_NOTE);
     expect(testAction.note).toEqual(item);
