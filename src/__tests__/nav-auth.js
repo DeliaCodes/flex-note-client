9bc25ad4535adc6107c4e9765835de84c194f9de
deliacodes <deliacodes@gmail.com>
2019-02-27 17:20:52 -0800

    Correcting smoketest to account for lack of Router from App.

--- src/__tests__/nav-auth.js
+++ src/__tests__/nav-auth.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import NavAuth from "../Nav-Auth";
 import store from "../store";
 import { Provider } from "react-redux";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Provider store={store}><NavAuth /></Provider>, div);
+  ReactDOM.render(<Provider store={store}><Router><NavAuth /></Router></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
